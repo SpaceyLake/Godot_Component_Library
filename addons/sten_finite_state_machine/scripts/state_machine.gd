fb37f29e4ca7a640f1446c1850a849f648ab573c
Savalige <matsbennervall@gmail.com>
2023-10-08 16:25:30 +0200

    Some MP stuff and State machines

---
+++ addons/sten_finite_state_machine/scripts/state_machine.gd
@@ -0,0 +1,40 @@
+extends Node
+class_name StateMachine
+
+@export var inital_state:State
+
+var current_state:State
+var states:Dictionary = { }
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for child in get_children():
+		if child is State:
+			states[child.name.to_lower()] = child
+			child.transition.connect(on_state_transition)
+	
+	if inital_state:
+		inital_state.enter()
+		current_state = inital_state
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if current_state:
+		current_state.update(delta)
+
+func _physics_process(delta):
+	if current_state:
+		current_state.physics_update(delta)
+
+func on_state_transition(state, new_state_name):
+	if state != current_state:
+		return
+	
+	var new_state = states.get(new_state_name.to_lower())
+	if !new_state:
+		return
+	
+	if current_state:
+		current_state.exit()
+	
+	new_state.enter()
